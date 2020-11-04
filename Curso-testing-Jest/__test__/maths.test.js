9be3cfb7960071da9865653532a872e49c2aa708
BbeatrizSI <bbeatriz@gmail.com>
2020-11-04 14:35:58 +0100

    platzi course testing with jest

---
+++ Curso-testing-Jest/__test__/maths.test.js
@@ -0,0 +1,17 @@
+import { TestScheduler } from 'jest';
+import {sumar, multiplicar, restar, dividir} from '../maths';
+
+describe('Calculos matemáticos', () => {
+    test('Prueba de sumas', () => {
+        expect(sumar(1,1)).toBe(2);
+    });
+    test('Prueba de multiplicaciones', () => {
+        expect(multiplicar(2,2)).toBe(4);
+    });
+    test('Prueba de restas', () => {
+        expect(restar(3,1)).toBe(2);
+    });
+    test('Prueba de divisiones', () => {
+        expect(dividir(4,2)).toBe(2);
+    });
+});
---
+++ Curso-testing-Jest/maths.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// funciones para realizar las pruebas
+
+export const sumar = (a,b) => {
+    return a + b;
+}
+
+export const multiplicar = (a,b) => {
+    return a * b;
+}
+
+export const restar = (a,b) => {
+    return a - b;
+}
+
+export const dividir = (a,b) => {
+    return a / b;
+}
